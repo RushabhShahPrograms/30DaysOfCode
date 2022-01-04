f9b09ef8f5ce322b3cf3d1c45be99ab802370cbb
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2022-01-04 16:06:07 +0530

    Update frame it up.py

--- Day 2/frame it up.py
+++ Day 2/frame it up.py
@@ -1,2 +1,27 @@
+'''
+Given a decimal number as input, we need to write a program to convert the given decimal number into equivalent binary number.
+
+Input Format
+7
+
+Constraints
+0
+
+Output Format
+111
+
+Sample Input 0
+7
+
+Sample Output 0
+111
+
+Sample Input 1
+11
+
+Sample Output 1
+1011
+'''
+
 num = int(input())
 print("{0:b}".format(int(num)))
