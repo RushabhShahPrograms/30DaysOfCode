2049b16a4069f3372b3b0a1ad432fca35901c35c
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2022-01-04 16:04:58 +0530

    Update unfriendly number.py

--- Day 2/unfriendly number.py
+++ Day 2/unfriendly number.py
@@ -1,3 +1,29 @@
+'''
+Raam is superstitious. He will only use a number as a roll number if all digits of the number are different(i.e. no digits are repeated)
+Return boolean string as output, print true if all digits are different and false if there are repetitions.
+Input Format
+983
+
+Constraints
+0
+
+Output Format
+true
+
+Sample Input 0
+9838
+
+Sample Output 0
+The number is unlucky.
+
+Sample Input 1
+1234
+
+Sample Output 1
+The number is lucky.
+'''
+
+
 def lucky(str):
     for i in range(len(str)):
         for j in range(i+1,len(str)):
