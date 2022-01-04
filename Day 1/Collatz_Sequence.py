250f9af1eb7cd90ddaf66accadb02a0054c49d35
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2022-01-04 16:34:40 +0530

    Update Collatz_Sequence.py

--- Day 1/Collatz_Sequence.py
+++ Day 1/Collatz_Sequence.py
@@ -1,3 +1,30 @@
+'''
+The Collatz sequence is generated sequentially where
+
+n = n / 2 if n is even
+n = 3 * n + 1 if n is odd
+And the sequence ends if n = 1
+N = 11;
+Output: 15
+Explanation:
+The Collatz sequence is: [11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1] and its length is 15.
+
+Input Format
+Input is simple number. Output the size of colatz sequence. 6
+
+Constraints
+0 < n < 10000
+
+Output Format
+9
+
+Sample Input 0
+6
+
+Sample Output 0
+9
+'''
+
 class Solution:
    def solve(self, num):
       if(num == 0):
