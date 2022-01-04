fb86ee05c1502b8deac84ddbd5ce00aec834a0a4
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2022-01-04 16:35:33 +0530

    Update Story_Of_N.py

--- Day 1/Story_Of_N.py
+++ Day 1/Story_Of_N.py
@@ -1,3 +1,23 @@
+'''
+Raam is superstitious. He will only use a particular digit of number as a roll number if its sum of digits is 11. Find those numbers having sum 11.
+Here, find the Nth number having sum 11. So, in short: Given an integer value n, find out the n-th positive integer whose sum is 11.
+
+Input Format
+2
+
+Constraints
+0 < n < 50
+
+Output Format
+38
+
+Sample Input 0
+3
+
+Sample Output 0
+47
+'''
+
 import itertools
 def findNth(n):
     count = 0
